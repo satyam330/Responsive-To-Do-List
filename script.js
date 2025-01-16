466821b180e41f72efccd1481cde1e1c2c78746d
satyam330 <satyamjha9132@gmail.com>
2025-01-16 17:29:49 +0530

    Initial commit of To-Do List project

---
+++ script.js
@@ -0,0 +1,33 @@
+const inputBox =document.getElementById('inputBox');
+const addBtn =document.getElementById('addBtn');
+const todoList =document.getElementById('todoList');
+
+const addTodo = () =>{
+    const inputText = inputBox.value.trim();
+    if(inputText.length <= 0){
+        alert("You Must Write Something in Your To Do");
+        return false;
+    }
+
+    //Creating p tag !!
+    const li =document.createElement("li");
+    const p = document.createElement("p");
+    p.innerHTML =inputText;
+    li.appendChild(p);
+
+    //Creating delete Btn
+    const deleteBtn =document.createElement("button");
+    deleteBtn.innerHTML = "Remove";
+    li.appendChild(deleteBtn);
+
+    //Creating edit Btn
+    const editBtn =document.createElement("button");
+    editBtn.innerHTML = "Edit";
+    li.appendChild(editBtn);
+
+    
+    todoList.appendChild(li);
+    inputBox.value= "";
+}
+addBtn.addEventListener('click',addTodo);
+
